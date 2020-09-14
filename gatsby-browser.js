93b31c3259739177429a71344d620c461fc797b1
Nithin David Thomas <webofnithin@gmail.com>
2020-09-14 17:10:25 +0530

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppProvider from 'store/provider';
 import wrapPageElementWithTransition from 'helpers/wrapPageElement';
 
 // React Context in Browser
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({ element }) => {
+   useEffect(() => {
+    (function(d,t) {
+      var BASE_URL = "https://chatwoot-mobile-app-test.herokuapp.com";
+      var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src= BASE_URL + "/packs/js/sdk.js";
+      s.parentNode.insertBefore(g,s);
+      g.onload=function(){
+        window.chatwootSDK.run({
+          websiteToken: 'o8uTRTCKYcuKqWcRcNirVhdE',
+          baseUrl: BASE_URL
+        })
+      }
+    })(document,"script");
+   }, []);
+  
   return <AppProvider>{element}</AppProvider>;
 };
 
